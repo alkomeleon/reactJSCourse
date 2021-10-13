cdb45990c731d11f1d316cd68cf19914e757dab9
alkomeleon <68281265+alkomeleon@users.noreply.github.com>
2021-10-13 21:57:43 +0300

    dz2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './style.css';
 
-export function App({ children, name, component, msgText }) {
+function renderMessage(msg, i){
+    let msgClass = msg.author==='user'?'userMsg':'botMsg';
+    return (
+        <p key={i} className={'msg '+msgClass}>
+            {msg.author}: {msg.message}
+        </p>);
+}
+
+export function App(props) {
+    const [messageList, setMessageList] = useState([]);
+    const [messageText, setMessageText] = useState('');
+
+    function sendMessage() {
+        if(messageText.length===0){
+            return false;
+        }
+        setMessageList([...messageList, {author: 'user', message: messageText}]);
+        setMessageText('');
+    }
+
+    useEffect(() => {
+        if (messageList.length === 0) return false;
+        if (messageList[messageList.length-1].author === 'bot') return false;
+        setTimeout(()=>setMessageList((messageList)=>[...messageList, {author: 'bot', message: 'hello'}]), 1000);
+    }, [messageList]);
 
     return (
         <div className="App">
-            <div>hello react, {name}</div>
-            <div>{children}</div>
-            <AppClass name={name} />
-            <Message msg={msgText} msg2={msgText} msg3={msgText}/>
-            {component}
+            <div className="chat">
+                {messageList.map((msg, i) => renderMessage(msg, i))}
+            </div>
+            <input type="text" id="msg_input" value={messageText} onChange={(event)=>setMessageText(event.target.value)}/>
+            <button onClick={sendMessage}>Отправить</button>
         </div>
     );
 }
 
-export class AppClass extends React.Component {
-    render() {
-        // this.props
-        const { name } = this.props;
-
-        return <div className="App">AppClass {name}</div>;
-    }
-}
-export function Message(props) {
-    return (
-        <div className="Message">
-            Сообщение: {props.msg}
-        </div>
-    )
-}
+export default App;
